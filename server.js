666b808b49af612756c3f62f723518b914ae583e
nathanrdvnk <nathan.radivoniuk@gmail.com>
2025-10-16 12:00:59 +0200

    modification route api/todos

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
-app.use("/api/tasks", todoRoutes);
+app.use("/api/todos", todoRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server lancé sur le port ${PORT}`));
+app.listen(PORT, () => console.log(`Server lancé sur http://localhost:${PORT}`));
