0a031e893138142bb5f1de4e6091b8d2781e89e0
nathanrdvnk <nathan.radivoniuk@gmail.com>
2025-10-16 10:17:32 +0200

    Adding server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import todoRoutes from "./src/routes/todoroutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/tasks", todoRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server lancé sur le port ${PORT}`));
