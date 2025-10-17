ddd9d52b8cf500e396b65effaf6a40970a18be75
nathanrdvnk <nathan.radivoniuk@gmail.com>
2025-10-17 12:23:31 +0200

    Modif todocontroller for postgre

--- src/controllers/todocontroller.js
+++ src/controllers/todocontroller.js
@@ -3,21 +3,40 @@ import Todo from "../model/todoModel.js";
 const todo = new Todo();
 
 export default class TodoController {
-  static list(req, res) {
-    res.json(todo.getAll());
+  static async list(req, res) {
+    try {
+      const todos = await todo.getAll();
+      res.json(todos);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erreur serveur" });
+    }
   }
 
-  static add(req, res) {
+  static async add(req, res) {
     const { title } = req.body;
-    if (!title) return res.status(400).json({ error: "Le titre est requis" });
-    const newTask = todo.addTask(title);
-    res.status(201).json(newTask);
+    if (!title) return res.status(400).json({ error: "Le titre nécessaire" });
+
+    try {
+      const newTask = await todo.addTask(title);
+      res.status(201).json(newTask);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erreur serveur" });
+    }
   }
 
-  static delete(req, res) {
+  static async delete(req, res) {
     const id = parseInt(req.params.id);
-    const deletedTask = todo.deleteTask(id);
-    if (deletedTask) return res.status(404).json({ error: "Tâche introuvable"});
-    res.json({ message: "Tâche supprimée" });
+    try {
+      const deletedTask = await todo.deleteTask(id);
+      if (!deletedTask) {
+        return res.status(404).json({ error: "Tâche introuvable" });
+      }
+      res.json({ message: "Tâche supprimée" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Erreur serveur" });
+    }
   }
 }
