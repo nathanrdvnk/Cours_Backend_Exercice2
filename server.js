2f5eafec450e579c22d56a70b53ec04496a8f8bc
nathanrdvnk <nathan.radivoniuk@gmail.com>
2025-11-02 22:49:01 +0100

    modification of server.js

--- server.js
+++ server.js
@@ -1,16 +1,50 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import todoRoutes from "./src/routes/todoroutes.js";
-
-dotenv.config();
-
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const { connectMongoDB, connectPostgreSQL } = require('./src/config/db');
+
+//Middleware
 app.use(cors());
 app.use(express.json());
 
-//Routes
-app.use("/api/todos", todoRoutes);
+const mode = process.env.MODE || 'mongo';
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server lancé sur http://localhost:${PORT}`));
+let dbClientPromise;
+
+//ConnexionBDD
+if (mode === 'mongo') {
+  dbClientPromise = connectMongoDB();
+} else if (mode === 'postgres') {
+  dbClientPromise = connectPostgreSQL();
+} else {
+  console.error('Mode inconnu, utilisez "mongo" ou "postgres"');
+  process.exit(1);
+}
+
+//Routes MongoDB
+if (mode === 'mongo') {
+  const mongoRoutes = require('./src/controllers/functions');
+  app.use('/functions', mongoRoutes);
+}
+
+//Routes PostgreSQL
+if (mode === 'postgres') {
+  (async () => {
+    try {
+      const { initTodo, default: TodoController } = require('./src/controllers/functions2');
+      await initTodo();
+
+      //Routes PostgreSQL
+      app.get('/todos', TodoController.list);
+      app.post('/todos', TodoController.add);
+      app.delete('/todos/:id', TodoController.delete);
+
+    } catch (err) {
+      console.error('Erreur d’initialisation PostgreSQL :', err.message);
+      process.exit(1);
+    }
+  })();
+}
+app.listen(3000, () => console.log(`Server started with : ${mode.toUpperCase()}`));
