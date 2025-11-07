c0cd8fe077311d29130349b4f5f072ed44148bb1
nathanrdvnk <nathan.radivoniuk@gmail.com>
2025-11-07 10:14:20 +0100

    add an 's' to the folder

---
+++ src/models/todomodel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Infos', schema)
